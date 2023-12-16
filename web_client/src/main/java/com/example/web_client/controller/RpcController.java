e89f75ecd8183dc02fc0c0c39698aed7bb0baee4
jinc <sophistnerd@163.com>
2023-12-17 01:04:10 +0800

    [Feature]实现简单的grpc通信

---
+++ web_client/src/main/java/com/example/web_client/controller/RpcController.java
@@ -0,0 +1,32 @@
+package com.example.web_client.controller;
+
+import com.silhouette.rpcinfo.RpcInfo;
+import com.silhouette.rpcservice.RpcServiceGrpc;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Iterator;
+import java.util.UUID;
+
+@Slf4j
+@RestController
+@RequestMapping("rpc")
+public class RpcController {
+
+    @Autowired
+    RpcServiceGrpc.RpcServiceBlockingStub rpcServiceBlockingStub;
+
+    @GetMapping("client")
+    public Object callRemote(){
+        Iterator<RpcInfo.RespInfo> callRemote = rpcServiceBlockingStub.remoteCall(RpcInfo.ReqInfo.newBuilder()
+                .setReqId(UUID.randomUUID().toString())
+                .setMsg("call remote")
+                .build()
+        );
+        callRemote.forEachRemaining(item -> log.info("receive remote resp: {}", item));
+        return "success";
+    }
+}
--- web_server/src/main/java/com/example/web_base/controller/RpcController.java
+++ web_server/src/main/java/com/example/web_base/controller/RpcController.java
@@ -1,6 +1,8 @@
 package com.example.web_base.controller;
 
+import com.example.web_base.service.RpcServiceImpl;
 import com.silhouette.rpcinfo.RpcInfo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/rpc")
 public class RpcController {
+
+    @Autowired
+    RpcServiceImpl rpcService;
 
     @GetMapping("call")
     public Object callRpc(){
