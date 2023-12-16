42555c82c77441009a6faa502a1824334e36a3e0
jinc <sophistnerd@163.com>
2023-12-16 21:34:52 +0800

    [Init]init repo

---
+++ web_base/src/main/java/com/example/web_base/WebBaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.web_base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebBaseApplication.class, args);
+    }
+
+}
