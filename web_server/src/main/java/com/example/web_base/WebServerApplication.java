ac7d230d2d3ca0e49d82f4dfec9b064aed86dd30
jinc <sophistnerd@163.com>
2023-12-17 00:40:31 +0800

    [Optimize]命名规范

--- web_server/src/main/java/com/example/web_base/WebServerApplication.java
+++ web_server/src/main/java/com/example/web_base/WebServerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebBaseApplication {
+public class WebServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WebBaseApplication.class, args);
+        SpringApplication.run(WebServerApplication.class, args);
     }
 
 }
