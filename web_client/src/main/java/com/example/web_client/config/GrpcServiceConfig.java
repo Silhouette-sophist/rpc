f950c00ebcdd1bd3c7518a558516e88e7c6f5a45
jinc <sophistnerd@163.com>
2023-12-17 01:17:56 +0800

    [Optimize]抽离grpc端口

--- web_client/src/main/java/com/example/web_client/config/GrpcServiceConfig.java
+++ web_client/src/main/java/com/example/web_client/config/GrpcServiceConfig.java
@@ -3,14 +3,16 @@ package com.example.web_client.config;
 import com.silhouette.rpcservice.RpcServiceGrpc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GrpcServiceConfig {
+
     @Bean
-    public ManagedChannel getChannel() {
-        return ManagedChannelBuilder.forAddress("localhost", 9090)
+    public ManagedChannel getChannel(@Value("${grpc.server.port}") int grpcServerPort) {
+        return ManagedChannelBuilder.forAddress("localhost", grpcServerPort)
                 .usePlaintext()
                 .build();
     }
